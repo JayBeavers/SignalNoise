65e164b77a3482d461e6342e2c67073a5b45b338
Jay Beavers <jay@hikinghomeschoolers.org>
2021-08-25 18:06:26 -0700

    Update the app for newer Visual Studio, .NET 4.8, and use the ToltTech GazeInput library

--- SignalNoise/MainWindow.xaml.cs
+++ SignalNoise/MainWindow.xaml.cs
@@ -1,38 +1,42 @@
-﻿using EyeXFramework;
-using Microsoft.Research.EyeGazeMouse;
+﻿using Microsoft.Research.EyeGazeMouse;
 using System;
 using System.Windows;
 using System.Windows.Threading;
+using ToltTech.GazeInput;
 
 namespace SignalNoise
 {
     public partial class MainWindow
     {
+        private GazeStats _gazeStats;
+        private DispatcherTimer _timer;
+        private IGazeDevice _gazeDevice;
+
         public MainWindow()
         {
             InitializeComponent();
             Loaded += MainWindow_Loaded;
         }
 
-        private EyeXHost _eyexHost;
-        private GazeStats _gazeStats;
-        private DispatcherTimer _timer;
-
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             _gazeStats = new GazeStats(90);
 
-            _eyexHost = new EyeXHost();
-            var eyePositionDataStream = _eyexHost.CreateEyePositionDataStream();
-            eyePositionDataStream.Next += eyePositionDataStream_Next;
-            _eyexHost.Start();
+            _gazeDevice = GazeDeviceDetector.FindGazeDevice();
+            _gazeDevice.GazePoint += GazeDevice_GazePoint;
+            _gazeDevice.Error += GazeDevice_Error;
 
             _timer = new DispatcherTimer(TimeSpan.FromSeconds(3), DispatcherPriority.Normal, UpdateStats, Dispatcher);
         }
- 
-        void eyePositionDataStream_Next(object sender, EyePositionEventArgs e)
+
+        private void GazeDevice_Error(object sender, MessageEventArgs e)
         {
-            _gazeStats.Update(e.LeftEye.X, e.LeftEye.Y);
+            MessageBox.Show(e.Message);
+        }
+
+        private void GazeDevice_GazePoint(object sender, GazeEventArgs e)
+        {
+            _gazeStats.Update(e.Screen.X, e.Screen.Y);
         }
 
         void UpdateStats(object o, EventArgs ea)
